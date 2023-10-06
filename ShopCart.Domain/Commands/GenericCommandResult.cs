f3178fd03e8ca76bc3efd3ae660cb82aa04bc0d7
Ronaldo Bueno <ronaldobueno2002@gmail.com>
2023-10-06 19:22:18 -0300

    Adicionando o modelo de comando para retornar em todas as requisições

---
+++ ShopCart.Domain/Commands/GenericCommandResult.cs
@@ -0,0 +1,17 @@
+﻿using ShopCart.Domain.Commands.Contracts;
+
+namespace ShopCart.Domain.Commands;
+
+public class GenericCommandResult : ICommandResult
+{
+    public GenericCommandResult(bool success, string message, object data)
+    {
+        Success = success;
+        Message = message;
+        Data = data;
+    }
+
+    public bool Success { get; private set; }
+    public string Message { get; private set; }
+    public object Data { get; private set; }
+}
